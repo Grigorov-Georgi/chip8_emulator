5793d512e8d5bf9296ef4dcef2eb523d2aab0514
Grigorov-Georgi <grigorov_georgi@yahoo.com>
2025-01-03 20:34:21 +0200

    initialize basic emulator structure

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+
+const START_ADDR: u16 = 0x200;
+
+pub struct Emu {
+    pc: u16, // program counter
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16, // RAM indexing register
+    stack: [u16; STACK_SIZE], // this can be implemented with std::collections::VecDeque and without sp
+    sp: u16, // stack pointer
+    keys: [bool; NUM_KEYS],
+    dt: u8, // delay timer
+    st: u8, // sound timer
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Emu {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            stack: [0; STACK_SIZE],
+            sp: 0,
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
     }
 }
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,6 +1,3 @@
-use chip8_core;
-
 fn main() {
     println!("Hello, world!");
-    println!("{}", chip8_core::add(1, 2));
 }
